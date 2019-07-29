e1c4e184e81a18a0eecbdcea25c15c618abc8273
Froyd <FroydCod3r@users.noreply.github.com>
2019-07-29 01:19:03 -0300

    Update enquete_zoada.py

--- enquete_zoada.py
+++ enquete_zoada.py
@@ -6,18 +6,19 @@ headers = {"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
             "Accept": "*/*",
             "Accept-Encoding":"gzip, deflate",
             "X-Requested-With": "XMLHttpRequest",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0",
-            "Cookie":"__atutc=; __atuuc=5185443; _ga=GA1.2.1114325762.1564363850; _gid=GA1.2.1706429745.1564363850; _gat_gtag_UAlgum001217_1=1; hibext_instdsigdipv2=1"}
+            "Referer": "https://strawpoll.com/7cpe4e95",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:67.0) Gecko/20100101 Firefox/67.0"}
 
 cont_request = 0
 for i in range(100):
       r = requests.post("https://strawpoll.com/vote", params=pt, headers=headers)
-      time.sleep(2)
+      time.sleep(5)
       print('Sucess!')
       cont_request += 1
-      print('Numero de requests %f0' % cont_request)
+      print('Numero de requests %i' % cont_request)
+      #Reinciando o loop para a proxima votacao ocorrer com outro IPv4
       os.system('systemctl restart tor')
       print('Reiniciando o tor')
-      time.sleep(7)
+      time.sleep(10)
 
 
